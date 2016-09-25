b05eab853448c1a49c7773a0d854999a490933f6
Yating <yz1339@nyu.edu>
2016-09-25 14:24:50 -0400

    add an interactive button

---
+++ startup-systems/javascripts/script.js
@@ -0,0 +1,33 @@
+// window.onload = function() {
+ 
+//     alert( "welcome" );
+ 
+// };
+
+$(document).ready(function() {
+ 
+    $("button").click(function( event ) {
+    	$.ajax({
+    		url: "https://api.github.com/users/yz1339",
+    		type: "GET",
+    		dataType: "json",
+    	})
+    	.done(function(json){
+    		console.log(json);
+    		$( "<p class=\"red\">" ).text("My github username is: " + json.login ).appendTo( "body" );
+     		$( "<p class=\"red\">").html("Details: " + json.url ).appendTo( "body" );
+    	})
+    	.fail(function( xhr, status, errorThrown ) {
+		    alert( "Sorry, there was a problem!" );
+		    console.log( "Error: " + errorThrown );
+		    console.log( "Status: " + status );
+		    console.dir( xhr );
+		})
+ 		.always(function( xhr, status ) {
+    		alert( "The request is complete!" );
+  		});
+        
+ 
+    });
+ 
+});
