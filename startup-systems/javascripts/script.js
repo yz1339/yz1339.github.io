53b5e06280e66c4a93b1abfdc64af0b6ac700fa5
Yating <yz1339@nyu.edu>
2016-09-25 15:36:34 -0400

    empty div before reclick

--- startup-systems/javascripts/script.js
+++ startup-systems/javascripts/script.js
@@ -7,15 +7,17 @@
 $(document).ready(function() {
  
     $("button").click(function( event ) {
+    	var div = $("div#button_clicked");
+    	div.empty(); //To prevent endless appending after several clicks
     	$.ajax({
     		url: "https://api.github.com/users/yz1339",
     		type: "GET",
     		dataType: "json",
     	})
     	.done(function(json){
-    		console.log(json);
-    		$( "<p class=\"red\">" ).text("My github username is: " + json.login ).appendTo( "body" );
-     		$( "<a>").attr("href", json.url).appendTo( "body" );
+    		console.log(div);
+    		$( "<p class=\"red\">" ).text("My github username is: " + json.login ).appendTo(div);
+     		$( "<a>").attr("href", json.url).appendTo(div);
      		$("<p class=\"red\">").text('Details').appendTo("a")
     	})
     	.fail(function( xhr, status, errorThrown ) {
@@ -27,8 +29,6 @@ $(document).ready(function() {
  		.always(function( xhr, status ) {
     		alert( "The request is complete!" );
   		});
-        
- 
     });
  
 });
